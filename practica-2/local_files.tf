1127b016fa0d33b9164c7275f11222448c366b8e
Paúl Guevara <pauljgl2008@gmail.com>
2026-02-07 14:54:43 -0500

    practica-2: applying principle DRY, using count

--- practica-2/local_files.tf
+++ practica-2/local_files.tf
@@ -1,4 +1,5 @@
 resource local_file productos {
+    count = 5
     content = "Lista de productos para el mes proximo"
-    filename = "productos-${random_string.sufijo.id}.txt"
+    filename = "productos-${random_string.sufijo[count.index].id}.txt"
 }
--- practica-2/random.tf
+++ practica-2/random.tf
@@ -1,4 +1,5 @@
 resource random_string sufijo {
+  count = 5
   length  = 4
   special = false
   upper = false
